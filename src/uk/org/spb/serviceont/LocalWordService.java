3a1b52dcbde9392ac8aab4a02a8323d9111df47d
Prabash Balasuriya <prabashbala@yahoo.com>
2014-12-22 17:16:41 +0000

    attempt to kill player from the notification

--- src/uk/org/spb/serviceont/LocalWordService.java
+++ src/uk/org/spb/serviceont/LocalWordService.java
@@ -4,7 +4,10 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.res.AssetFileDescriptor;
 import android.media.MediaPlayer;
 import android.os.Binder;
@@ -16,6 +19,8 @@ public class LocalWordService extends Service {
     private final IBinder mBinder = new MyBinder();
     // media player
     private MediaPlayer player;
+    
+    private static final String NOTIFICATION_DELETED_ACTION = "NOTIFICATION_DELETED";
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
@@ -40,21 +45,49 @@ public class LocalWordService extends Service {
     public void onCreate(){
 
 	Log.d("LocalWordService","oncreate notification*************");
-       NotificationManager mNM = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
-       Intent intent1 = new Intent(this.getApplicationContext(), TimePickerActivity.class);
+       /*NotificationManager mNM = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+       Intent intent1 = new Intent(this.getApplicationContext(), LocalWordService.class);
        //PendingIntent pIntent = PendingIntent.getActivity(this, 0, intent1, 0);
        PendingIntent pIntent = PendingIntent.getActivity(this, 0, intent1, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_ONE_SHOT);
-
-       Notification mNotify = new NotificationCompat.Builder(this)
+         Notification mNotify = new NotificationCompat.Builder(this)
                .setAutoCancel(true)
                .setContentTitle("Playing Pirith Chanting audio")
                .setContentText("Pirith Chanting app now playing pirith audio file")
                .setSmallIcon(R.drawable.ic_stat_notify_msg)
-               .setContentIntent(pIntent)
+               //.setContentIntent(pIntent)
+               .setDeleteIntent(pIntent)
                .build();
 
        mNM.notify(1, mNotify);
        Log.d("LocalWordService","done oncreate notification*************");
+       */
+       final BroadcastReceiver receiver = new BroadcastReceiver() {
+
+		@Override
+		public void onReceive(Context context, Intent intent) {
+		    // TODO Auto-generated method stub
+		    Log.d("BroadcastReceiver","innerclass");
+		   if(player.isPlaying()){
+		       Log.d("BroadcastReceiver","innerclass stoping player");
+		       player.stop();
+		   }
+	            unregisterReceiver(this);
+		}
+	    };
+	    NotificationManager mNM = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+	    Intent intent = new Intent(NOTIFICATION_DELETED_ACTION);
+	        PendingIntent pendintIntent = PendingIntent.getBroadcast(this, 0, intent, 0);
+	        registerReceiver(receiver, new IntentFilter(NOTIFICATION_DELETED_ACTION));
+	        Notification n = new Notification.Builder(this)
+	        	 .setAutoCancel(true)
+	                 .setContentTitle("Playing Pirith Chanting audio")
+	                 .setContentText("Pirith Chanting app now playing pirith audio file")
+	                 .setDeleteIntent(pendintIntent)
+	                 .setSmallIcon(R.drawable.ic_stat_notify_msg)
+	         . build();
+	        mNM.notify(0, n);
+	        Log.d("LocalWordService","notification set");
+     
     }
     // play a song
     public void playSong() {
