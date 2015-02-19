d600fd44d5f60e1ba8eb54918f8550a1ba67828f
Prabash <Prabash@Prabash-VAIO>
2015-02-19 22:23:13 +0000

    set the expansion file path according to nexus 10 path/storage/emulated/0/Android/obb/main.2.uk.org.spb.serviceont.obb

--- src/uk/org/spb/serviceont/util/ApplicationCofig.java
+++ src/uk/org/spb/serviceont/util/ApplicationCofig.java
@@ -8,5 +8,5 @@ package uk.org.spb.serviceont.util;
 public class ApplicationCofig {
     public static String MP3FILENAME="Ruwan.mp3";
     public static String APPLICATION_VERSION_NUMBER = "1";
-    public final static String EXPANSION_FILE_HOME_DIR = "/Android/obb/";
+    public final static String EXPANSION_FILE_HOME_DIR = "/Android/obb/uk.org.spb.serviceont/";
 }
