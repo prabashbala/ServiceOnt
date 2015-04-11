661fab2f76ee9dcbc2f311b60b38789414f3f16e
prabashbala@yahoo.com <prabashbala@yahoo.com>
2015-04-11 19:06:10 +0100

    introduce play button

--- src/uk/co/balasuriya/serviceont/SelectAudioFileActivity.java
+++ src/uk/co/balasuriya/serviceont/SelectAudioFileActivity.java
@@ -1,16 +1,21 @@
 package uk.co.balasuriya.serviceont;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import uk.co.balasuriya.serviceont.util.ApplicationContext;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.AdapterView;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -18,10 +23,7 @@ public class SelectAudioFileActivity extends Activity {
 
     public static final String ALARM_DATA = "alarmdata";
     /** Private members of the class */
-    private ImageView displayTime;
     private ListView listview;
-    private TextView noAlarmBackgroundImage;
-
 
     /** Called when the activity is first created. */
     @Override
@@ -29,35 +31,81 @@ public class SelectAudioFileActivity extends Activity {
 	super.onCreate(savedInstanceState);
 	setContentView(R.layout.activity_main1);
 	listview = (ListView) findViewById(R.id.audio_file_list);
-	
-	String[] values = new String[] { "Seth Piritha", "Sabbha papassa gatha", "Rathnamali gatha","Seevalie piritha"};
-	
+
+	String[] values = new String[] { "Seth Piritha", "Sabbha papassa gatha", "Rathnamali gatha", "Seevalie piritha" };
+
 	final ArrayList<String> list = new ArrayList<String>();
-	    for (int i = 0; i < values.length; ++i) {
-	      list.add(values[i]);
-	    }
-	    final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.audiolistlayout, R.id.label,
-		    list);
-		Log.d("onCreate:", "alarmdata:" + " Adaptor:" + adapter.getCount());
-		listview.setAdapter(adapter);
-		
-		listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+	for (int i = 0; i < values.length; ++i) {
+	    list.add(values[i]);
+	}
+	final ListViewArrayAdapter adapter = new ListViewArrayAdapter(this, R.layout.audiolistlayout,
+		R.id.play_audio_file, list);
+	Log.d("onCreate:", "alarmdata:" + " Adaptor:" + adapter.getCount());
+	listview.setAdapter(adapter);
 
-		    @Override
-		    public void onItemClick(AdapterView<?> parent, final View view, int position, long id) {
-			 final String item = (String) parent.getItemAtPosition(position);
-			 Log.d("onItemClick:",item);      
-			 Intent intent = new Intent(ApplicationContext.getAppContext(), TimePickerActivity.class);
+    }
 
-			 intent.putExtra("id", item);
+    class ViewHolder {
+	ImageButton button;
+	TextView title;
+    }
 
-			 startActivity(intent);
-		    }
-
-		    });
-
-
+    class ListViewArrayAdapter extends ArrayAdapter<String> {
 	
+	List<String> audiolist;
+
+	public ListViewArrayAdapter(Context context, int resource, int textViewResourceId, List<String> objects) {
+	    super(context, resource, textViewResourceId, objects);
+	    audiolist=objects;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+	    super.getView(position, convertView, parent);
+	    View v = convertView;
+	    ViewHolder viewHolder;
+
+	    if (v == null) {
+		LayoutInflater inflater = (LayoutInflater) ApplicationContext.getAppContext().getSystemService(
+			Context.LAYOUT_INFLATER_SERVICE);
+		v = inflater.inflate(R.layout.audiolistlayout, null);
+		viewHolder = new ViewHolder();
+		viewHolder.button = (ImageButton) v.findViewById(R.id.play_audio);
+		viewHolder.title = (TextView) v.findViewById(R.id.play_audio_file);
+		v.setTag(viewHolder);
+	    } else {
+		viewHolder = (ViewHolder) v.getTag();
+	    }
+
+	    if (viewHolder.title != null) {
+		viewHolder.title.setText(audiolist.get(position));
+		viewHolder.title.setTextColor(Color.BLACK);
+	    }
+
+	    viewHolder.button.setOnClickListener(new OnClickListener() {
+
+		@Override
+		public void onClick(View v) {
+		    Intent service = new Intent(ApplicationContext.getAppContext(), BackgroundMusicService.class);
+		    ApplicationContext.getAppContext().startService(service);
+		    Log.d("onCreate:", "");
+		}
+	    });
+
+	    v.setOnClickListener(new OnClickListener() {
+
+		@Override
+		public void onClick(View v) {
+		    Intent intent = new Intent(ApplicationContext.getAppContext(), TimePickerActivity.class);
+		    intent.putExtra("id", v.getId());
+		    startActivity(intent);
+		    Log.d("view onCreate:", "");
+		}
+	    });
+
+	    return v;
+	}
+
     }
 
 }
